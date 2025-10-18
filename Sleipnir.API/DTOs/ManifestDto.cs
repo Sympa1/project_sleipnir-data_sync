28de5191676d20903503cca20873a4648f0f1fbb
sympa <tristan.siegel@googlemail.com>
2025-10-18 22:56:57 +0200

    Manifest DTO hinzugefügt.

---
+++ Sleipnir.API/DTOs/ManifestDto.cs
@@ -0,0 +1,11 @@
+namespace Sleipnir.API.DTOs;
+
+public class ManifestDto
+{
+    public int size { get; set; }
+    public DateTime modifactionTime { get; set; }
+    public DateTime createdAt { get; set; }
+    public string hash { get; set; }
+    public string state { get; set; }
+    public DateTime updatedAt { get; set; }
+}
