c23f5dd8c6193f50dd30f7dd64d7ff1759ee1bc6
sympa <tristan.siegel@googlemail.com>
2025-10-26 19:39:50 +0100

    GetFilesToSyncService.cs fertiggestellt.

--- Sleipnir.API/Controllers/FileSyncController.cs
+++ Sleipnir.API/Controllers/FileSyncController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Sleipnir.API.DTOs;
+using Sleipnir.API.Services;
 
 namespace Sleipnir.API.Controllers;
 
@@ -9,17 +10,28 @@ public class FileSyncController : Controller
 {
     // TODO: Per Konstruktor eine Instanz von GetFilesToSyncService injizieren?
     //  ist die dann immer noch für nur einen Request?
+    private readonly GetFilesToSyncService _filesToSyncService;
+    
+    /// <summary>
+    /// Konstruktor für FileSyncController, der GetFilesToSyncService injiziert.
+    /// </summary>
+    /// <param name="filesToSyncService"></param>
+    public FileSyncController(GetFilesToSyncService filesToSyncService)
+    {
+        _filesToSyncService = filesToSyncService;
+    }
+    
+    
     [HttpPost]
     public async Task<IActionResult> PostManifestByClient([FromBody] List<ManifestDto> manifests)
     {
         if (manifests == null || manifests.Count == 0)
             return BadRequest("Manifest missing or invalid.");
         
-        //TODO: .env File hinzufügen.
-        
         // TODO: Implementierung Methoden zum Abgleich, welche Dateien geupdated werden müssen.
+        var filesToSync = _filesToSyncService.GetFilesToSync(manifests);
         
         // TODO: Ok richtig? Ich will ja dem Client eine Liste der zu syncenden Dateien zurückgeben.
-        return Ok(manifests.Count);
+        return Ok(filesToSync);
     }
 }
--- Sleipnir.API/Services/GetFilesToSyncService.cs
+++ Sleipnir.API/Services/GetFilesToSyncService.cs
@@ -14,15 +14,42 @@ public class GetFilesToSyncService
         _mySqlService = mySqlService;
     }
     
+    /// <summary>
+    /// Ermittelt, anhand der Manifestliste, die zu synchronisierenden Dateien.
+    /// </summary>
+    /// <param name="manifests">Liste: ManifestDto</param>
+    /// <returns>Liste: filesToSync</returns>
     public async Task<List<FilesToSyncDto>> GetFilesToSync(List<ManifestDto> manifests)
     {
         List<FilesToSyncDto> filesToSync = new List<FilesToSyncDto>();
-        // TODO: Implementierung DB Connection
         
-        // TODO: foreach über Manifests
-            // TODO: Prüfen ob Datei in DB existiert
-            // TODO: Wenn nicht, in neue Liste aufnehmen
+        // Ab .NET 8 sind keine geschweiften Klammern mehr notwendig. Dispose automatisch am Methodenende.
+        using var dbConnection = _mySqlService.OpenConnection();
 
-            return filesToSync;
+        foreach (var manifest in manifests)
+        {
+            using var dbCommand = dbConnection.CreateCommand();
+
+            if (manifest.state == "modified")
+            {
+                dbCommand.CommandText = "SELECT COUNT(file_id) FROM file WHERE state = 'modified' AND hash != @hash";
+                dbCommand.Parameters.AddWithValue("@hash", manifest.hash);
+
+                // await hinzufügen
+                var count = dbCommand.ExecuteScalar();
+
+                if (count != null && Convert.ToInt32(count) > 0)
+                {
+                    filesToSync.Add(new FilesToSyncDto
+                    {
+                        hash = manifest.hash,
+                        state = manifest.state,
+                        size = manifest.size
+                    });
+                }
+            }
+        }
+        
+        return filesToSync;
     }
 }
