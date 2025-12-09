0854e9ed20b5833dfcd6782a800b89f51f4a33f6
sympa <tristan.siegel@googlemail.com>
2025-12-09 12:22:08 +0100

    Update der Program.cs bzgl. des DbStartupCheckServices.

--- data_sync.API/Program.cs
+++ data_sync.API/Program.cs
@@ -1,6 +1,9 @@
 using data_sync.API.Services;
 
-// TODO: DbStartup fertigstellen und hier aufrufen.
+// TODO: DbStartup - es wird nur ein Table angelegt.
+// TODO: Scoping überprüfen ob Sinnvoll (bspw. FileLogService evtl. Singleton?). Oder aber weil ich nur einmal den
+//  DbStartupCheckService brauche, diesen außerhalb des Scopes instanziieren.
+// TODO: Was ist genau dieser Scope und wie funktioniert dieser?
 
 // .env einmalig beim Start laden via EnvLoadeService (sucht Projektstamm).
 // → Die Methode liest die .env Datei (falls vorhanden) und schreibt die Werte in das Prozess-Environment.
@@ -18,13 +21,20 @@ builder.Services.AddScoped<GetFilesToSyncService>();
 // Registriere MariaDbService als Scoped (wird pro Request neu erstellt und automatisch disposed)
 builder.Services.AddScoped<MariaDbService>();
 
+// Registriere DbStartupCheckService als Scoped (für DB-Startup-Checks)
+builder.Services.AddScoped<DbStartupCheckService>();
+
+// Registriere FileLogService als Singleton (eine Instanz für die gesamte App-Lebensdauer)
+//builder.Services.AddSingleton<FileLogService>();
+
 var app = builder.Build();
 
 // DB-Startup-Checks in einem Scope ausführen (Scoped Services korrekt verwenden)
 using (var scope = app.Services.CreateScope())
 {
     var dbStartup = scope.ServiceProvider.GetRequiredService<DbStartupCheckService>();
-    var log = scope.ServiceProvider.GetRequiredService<FileLogService>();
+    //var log = scope.ServiceProvider.GetRequiredService<FileLogService>();
+    FileLogService log = new FileLogService("Db-Startup.log");
     try
     {
         dbStartup.CheckDatabaseConnection();
