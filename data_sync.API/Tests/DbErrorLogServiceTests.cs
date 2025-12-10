3b02b183f0591e90a72e07e2186d8447a0e38311
sympa <tristan.siegel@googlemail.com>
2025-12-10 19:38:00 +0100

    WIP - DbErrorLogServiceTests.cs erfolgreich. Weitere TODOs gemäß DbErrorLogSerive.cs und folgenden Unit Test.

--- data_sync.API/Tests/DbErrorLogServiceTests.cs
+++ data_sync.API/Tests/DbErrorLogServiceTests.cs
@@ -9,4 +9,20 @@ namespace data_sync.API.Tests;
 public class DbErrorLogServiceTests
 {
     // Testet, ob LogDbErrorAsync eine Exception protokolliert
+
+    public void LogDbErrorAsync_ShouldLogException()
+    {
+        // Arrange
+        var mariaDbService = new MariaDbService();
+        var dbErrorLogService = new DbErrorLogService(mariaDbService);
+        var testException = new Exception("Test exception message");
+        var contextInfo = "UnitTest";
+
+        // Act
+        var logTask = dbErrorLogService.LogDbErrorAsync(testException, contextInfo);
+        logTask.Wait(); // Warten auf den Abschluss der asynchronen Methode
+
+        // Assert
+        // Hier
+    }
 }
--- data_sync.API/Tests/TestService.cs
+++ data_sync.API/Tests/TestService.cs
@@ -5,6 +5,18 @@ public class TestService
     // Steuert Tests und wird je nach Option in der Programm.cs ausgeführt
     public void ChoiceTest(int testVariante)
     {
-        // per switch case die gewünschte Testmethode aufrufen
+        switch (testVariante)
+        {
+            case 1:
+                DbErrorLogServiceTests dbErrorLogTests = new DbErrorLogServiceTests();
+                dbErrorLogTests.LogDbErrorAsync_ShouldLogException();
+                break;
+            case 2:
+                // Testvariante 2 ausführen
+                break;
+            default:
+                // Standardaktion oder Fehlerbehandlung
+                break;
+        }
     }
 }
