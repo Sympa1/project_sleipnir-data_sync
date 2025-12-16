ee7fca3b65a00cd8ea5a312622afc3b8a6bd8588
tux <sympa@tux.tux>
2025-12-16 12:38:50 +0100

    Weitere API Endpunkte vorbereitet.

--- data_sync.API/Controllers/SyncController.cs
+++ data_sync.API/Controllers/SyncController.cs
@@ -7,21 +7,22 @@ namespace data_sync.API.Controllers;
 // NOTE: Beim Download zum Client muss anschließend der Hashwert vom Client neu berechnet und in der ClientDB aktualisiert werden.
 
 [ApiController]
-[Route("api/sync/[controller]")]
-public class ManifestController : ControllerBase
+[Route("api/[controller]")]
+public class SyncController : ControllerBase
 {
     private readonly GetFilesToSyncService _filesToSyncService;
     
     /// <summary>
-    /// Konstruktor für ManifestController, der GetFilesToSyncService injiziert.
+    /// Konstruktor für SyncController, der GetFilesToSyncService injiziert.
     /// </summary>
     /// <param name="filesToSyncService"></param>
-    public ManifestController(GetFilesToSyncService filesToSyncService)
+    public SyncController(GetFilesToSyncService filesToSyncService)
     {
         _filesToSyncService = filesToSyncService;
     }
     
     [HttpPost]
+    [Route("manifest")]
     public async Task<IActionResult> PostManifestByClient([FromBody] List<ManifestEntryDto> manifests)
     {
         if (manifests.Count == 0)
@@ -33,5 +34,23 @@ public class ManifestController : ControllerBase
         // Gib dem Client die Liste der zu synchronisierenden Dateien zurück
         return Ok(filesToSync);
     }
+
+    [HttpPost]
+    [Route("upload")]
+    public async Task<IActionResult> UploadFile()
+    {
+        // Vorbereitungen zum Upload einer Datei
+        return null;
+    }
+    
+    [HttpGet]
+    [Route("download")]
+    public async Task<IActionResult> DownloadFile([FromQuery] string filePath)
+    {
+        // Vorbereitungen zum download einer Datei
+        return null;
+    }
+    
+    // TODO: Wie mache ich das mit dem löschen von Dateien?
 }
 
