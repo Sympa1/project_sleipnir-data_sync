d3e6c856df4454e9de9c30ca36cdebc457f3c5c3
sympa <tristan.siegel@googlemail.com>
2026-01-08 18:21:09 +0100

    WiP - Command Pattern implementieren.

---
+++ data_sync.CLI/src/backend/handlers/config_handler.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+class ConfigHandler:
+    def __init__(self, filename = "config.json"):
+        """
+        Initialisiert den ConfigHandler mit dem Pfad zur Konfigurationsdatei.
+        :param filename:
+        """
+        self.filename = filename
+        self.data = {}
+        self.load_json() # lädt automatisch die Config beim Erstellen des Objektes
+
+    def load_json(self):
+        """Liest das JSON File ein. Wenn die Datei nicht existiert, wird eine neue erstellt.
+        :return: None
+        """
+        if not os.path.exists(self.filename):
+            self.data = {} # Oder Standardwerte hier setzen
+            self.save_config()
+        else:
+            with open(self.filename, 'r') as f:
+                self.data = json.load(f)
+
+    def save_config(self):
+        """Speichert die Werte im JSON File.
+        :return: None
+        """
+        with open(self.filename, 'w') as f:
+            json.dump(self.data, f, indent=2)
+
+    def get_config(self, key):
+        """Gibt den Wert für den angegebenen Schlüssel zurück, oder None, wenn der Schlüssel nicht existiert.
+        :param key: Schlüssel der Konfiguration
+        :return: Wert der Konfiguration oder None
+        """
+        return self.data.get(key)
+
+    def set_config(self, key, value):
+        """Setzt den Wert für den angegebenen Schlüssel und speichert die Konfiguration.
+        :param key: Schlüssel der Konfiguration
+        :param value: Wert der Konfiguration
+        :return: None
+         """
+        self.data[key] = value
+        self.save_config()
---
+++ data_sync.CLI/src/backend/handlers/manifest_handler.py
@@ -0,0 +1,41 @@
+import json
+from datetime import datetime
+from .sqlite_handler import SqliteHandler
+from ..api_client import ApiClient
+from .config_handler import ConfigHandler
+
+
+class ManifestHandler:
+    def __init__(self):
+        self.db_handler = SqliteHandler()
+        self.config_handler = ConfigHandler()
+        self.api = ApiClient(self.config_handler.get_config("api_base_url"))
+
+    def create_manifest(self) -> str:
+        """
+        Erstellt das Manifest für die Datensynchronisation.
+        Holt alle Daten aus der DB, erstellt ein Manifest-Objekt und serialisiert es zu JSON.
+        :return: JSON-String des Manifests
+        """
+
+        # SQL Abfrage zum Abrufen aller Datensätze
+
+        # Ausfphrung der Abfrage und Speichern der Ergebnisse in einer Variable
+
+        # manifest Array initialisieren
+        # Manifest enthält alle Datensätze als Liste von Objekten
+        manifest = []
+
+        # Durchlaufen der Datensätze und Hinzufügen zum Manifest mitztels for Schleife
+
+        return manifest
+
+    def send_manifest(self):
+        """
+        Sendet das Manifest an die API.
+        :param: None
+        :return: API-Antwort als JSON
+        """
+        manifest = self.create_manifest()
+        response = self.api.post("/manifest", json=manifest)
+        return response.json()
